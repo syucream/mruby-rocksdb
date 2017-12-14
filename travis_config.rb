f9bca520dda9d412db74f294df0612f57edd54db
Ryo Okubo <syucream1031@gmail.com>
2017-12-15 00:20:33 +0900

    Fix travis config

--- travis_config.rb
+++ travis_config.rb
@@ -2,12 +2,13 @@ MRuby::Build.new do |conf|
   toolchain :gcc
 
   enable_debug
+  conf.gembox 'default'
   conf.enable_bintest
   conf.enable_test
+  conf.enable_cxx_abi
 
   # To test with rocksdb
   conf.linker.flags_after_libraries << "-lrocksdb"
 
-  conf.gembox 'default'
   conf.gem './'
 end
